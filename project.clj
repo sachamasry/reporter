9d56c2b7e906c78b5bfbfea0d29901281ec28e03
sacha <sacha@sachamasry.com>
2025-03-03 14:05:01 +0000

    Updated project version number, CHANGELOG and LICENSE files, effectively making the project MIT-licensed.
    FossilOrigin-Name: 3eb2af90c806153489ced298df2b65bd78b002bf729e37a97ca61ef15737e1b7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reporter "0.1.0-SNAPSHOT"
+(defproject reporter "0.1.0"
   :description "Clojure wrapper around Jasper Reports report-generation software"
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
