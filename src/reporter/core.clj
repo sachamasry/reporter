3bc7bd98d3d7ceb00b5cb1ef558fe07c0d3b2066
sacha <sacha@sachamasry.com>
2025-03-18 03:20:00 +0000

    Updated prototype report generation to be able to generate reports based on using the provided SQLite connection instead of Java Beans for the first time. The primary temporarily created table name is successfully read from the report jobs table, and is then injected into the report via a `${TABLE_NAME}` parameter. Updated `generate-report` function to work standalone, defining a new `process-job` function that reads all the relevant information from the job queue table to call the generation function with real template, report name and output format specifications, using them in producing the output file.
    FossilOrigin-Name: 4b81e2e8d8928804b8f75f45f674d4beead8027389f5327ff84a02254741b91b

--- src/reporter/core.clj
+++ src/reporter/core.clj
@@ -3,19 +3,26 @@
   needed for interfacing with the Java Reports library, and prototype code
   demonstrating a working report-generating solution."
   (:require
-            [clojure.java.io :as io]
-            [clojure.java.jdbc :as jdbc]
-            [cheshire.core :as json]
-            [clojure.string :as str]
-            [clj-bean.core :as bean])
+   [clojure.java.io :as io]
+   [clojure.java.jdbc :as jdbc]
+   [cheshire.core :as json]
+   [clojure.string :as str]
+   [clj-bean.core :as bean])
   (:import [net.sf.jasperreports.engine
             JasperCompileManager
             JasperFillManager
             JasperExportManager
-            JasperPrint]
+            JasperPrint
+            JRParameter]
+           [net.sf.jasperreports.engine.design JasperDesign]
+           ;; [net.sf.jasperreports.engine.xml JRXPathQueryExecuterFactory]
            [net.sf.jasperreports.engine JRPropertiesUtil]
-           [net.sf.jasperreports.engine.data JRBeanCollectionDataSource]
-           [java.util HashMap]))
+           [net.sf.jasperreports.engine.util JRXmlUtils]
+           [net.sf.jasperreports.engine JRDataSource]
+           [net.sf.jasperreports.engine.query JRJdbcQueryExecuterFactory]))
+
+           ;; [net.sf.jasperreports.engine.data JRBeanCollectionDataSource]
+           ;; [java.util HashMap]))
 
 (defn parse-json
   "Converts a JSON string into a native Clojure map."
@@ -32,11 +39,11 @@
   [template-path destination-path]
   (JasperCompileManager/compileReportToFile template-path destination-path))
 
-(defn fill-report
-  "Fills a compiled Jasper report with data."
-  [compiled-report-path data]
-  (let [datasource (JRBeanCollectionDataSource. data)]
-    (JasperFillManager/fillReport compiled-report-path nil datasource)))
+;; (defn fill-report
+;;   "Fills a compiled Jasper report with data."
+;;   [compiled-report-path data]
+;;   (let [datasource (JRBeanCollectionDataSource. data)]
+;;     (JasperFillManager/fillReport compiled-report-path nil datasource)))
 
 (defn export-to-pdf
   "Exports a filled Jasper report to a PDF file."
@@ -44,34 +51,45 @@
   (JasperExportManager/exportReportToPdfFile filled-report output-path)
   (println (str "PDF saved to: " output-path)))
 
-(defn generate-report [jrxml-path output-path report-data]
-  (let [jasper-report (JasperCompileManager/compileReport jrxml-path)
-        data-source (JRBeanCollectionDataSource. report-data)
-        parameters (HashMap.)]
+(defn generate-report [jrxml-path output-path db-connection data-table-name]
+  (let [compiled-report (JasperCompileManager/compileReport jrxml-path)
+        parameters (java.util.HashMap.)]
+    (.put parameters "TABLE_NAME" data-table-name)
     (JasperExportManager/exportReportToPdfFile
-     (JasperFillManager/fillReport jasper-report parameters data-source)
+     (JasperFillManager/fillReport compiled-report parameters db-connection)
      output-path)))
 
-(def db-connection {:classname "org.sqlite.JDBC"
-                    :subprotocol "sqlite"
-                    :subname "/Users/sacha/Development/elixir/klepsidra/db/reporter_dev.db"})  ;; Use the SQLite file that Elixir writes to
-                    ;; :subname "/Users/sacha/bin/klepsidra/db/reporter.db"})  ;; Use the SQLite file that Elixir writes to
+(def db-specification {:classname "org.sqlite.JDBC"
+                       :subprotocol "sqlite"
+                       :subname "/Users/sacha/Development/elixir/klepsidra/db/reporter_dev.db"})
+;; :subname "/Users/sacha/bin/klepsidra/db/reporter.db"})  
+
+;; Get a real JDBC connection
+(def db-connection (jdbc/get-connection db-specification))
 
 (defn list-defined-tables
-  []
   "Lists all tables defined in the database"
+  []
   (jdbc/query db-connection ["SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"]))
 
-(defn get-next-job []
+(defn get-next-job
   "Picks up next 'pending' job from the report job queue, returning the entire
   database record."
-  (first (jdbc/query db-connection ["SELECT * FROM report_jobs WHERE state = 'pending' ORDER BY inserted_at LIMIT 1"])))
+  []
+  (first (jdbc/query db-specification ["SELECT * FROM report_jobs WHERE state = 'available' ORDER BY inserted_at LIMIT 1"])))
 
 ;; Proposed code, to be tested
-(defn process-job [job]
-  (let [params [] ;; (json/read-str (:parameters job) :key-fn keyword)
-        output-path (str "output/report-" (:id job) ".pdf")]
-    (generate-report "resources/template.jrxml" output-path params)))
+(defn process-job [template-path db-connection job]
+  (let [report-name (:report_name job)
+        report-template (:report_template job)
+        output-format (:output_format job)
+        temporary-data-tables (:temporary_tables_created job)
+        primary-data-table-name (-> temporary-data-tables (parse-json) (:primary))
+        ;; parameters [] ;; (json/read-str (:parameters job) :key-fn keyword)
+        output-path (str "output/" report-template "." output-format)
+        ]
+    (generate-report template-path output-path db-connection primary-data-table-name)))
+
     ;; (jdbc/update! db-spec :report_jobs
     ;;               {:status "completed" :result_path output-path :updated_at (java.time.LocalDateTime/now)}
     ;;               ["id = ?" (:id job)])))
